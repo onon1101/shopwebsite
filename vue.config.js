f9ebfd54026f7e56f1d7b37ce3244e402ed671aa
Yihss <a2685744@gmail.com>
2024-10-17 02:54:35 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './shopwebsite',
+  baseUrl : './shopwebsite',
   assetsDir:'shopwebsite',
   outputDir:"dist",
 })
