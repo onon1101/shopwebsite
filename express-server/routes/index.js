7409ee1f69e33656aab28682dbffa3f37fab4f97
Yihss <a2685744@gmail.com>
2024-10-18 07:10:07 +0800

    資料庫密碼設定模式

--- express-server/routes/index.js
+++ express-server/routes/index.js
@@ -1,21 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-var db = {
-    host: 'localhost',
-    user: 'root',
-    password: 'gf5357485250',
-    database:'node.js-test',
-    port: 3306
-};
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  dp.connect();
-  dp.query('SELECT 12 + 34 AS result', function(err, rows, fields) {
-    if (err) throw err;
-    console.log('The result is: ', rows[0].result);
-}); 
+
   res.render('index', { title: 'Express' });
 });
 
