c8e3a15afec47dc07c5b77ae40b45171aacc3ac6
Yihss <a2685744@gmail.com>
2024-11-19 05:27:35 +0800

    首頁增加新商品

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,21 @@
 <template>
+
   <div class="home">
     <HeadMenu/>
     <div class = "slidshowimage">
       <Slidshowimage/>
     </div>
-
-
   </div>
   <div class ="newarrival">
       <h1>NEW ARRIVAL</h1>
       <h1>新商品</h1>
-  </div>
 
+        <div class = newproductlist v-for ="(item,index) in images" :key = "index">
+           <img :src = "item" alt = "">
+
+      </div>
+
+  </div>
 </template>
 
 <script>
@@ -23,21 +27,57 @@ export default {
   name: 'HomeView' ,
   components: {
     HeadMenu,Slidshowimage
+  },
+  data:function(){
+    return{
+      images:(function(){
+        let filearr = [] ;
+        for(let i = 1 ; i <=8 ; i++){
+          filearr[i-1] = require('../assets/test' + i + '.png');
+        }
+        return filearr;
+      })(),
+    }
   }
 }
 </script>
 
 <style scoped>
+
 .home .slidshowimage{
   position: relative;
   top:120px;
 }
 
+
+.newarrival{
+  position: relative;
+  top: 200px;
+  margin: 0px 200px;
+}
+
+
 .newarrival h1{
   font-family:'fantasy';
   color: rgba(149, 75, 219, 0.842);
-  position: relative;
-  top: 200px;
 }
 
+
+.newarrival .newproductlist{
+  
+  display:inline-block;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-items: flex-start;
+  margin-left: -80px;
+  margin-top: 20px;
+}
+
+.newarrival .newproductlist img{
+
+
+  max-width: 70%;
+  height: auto;
+
+}
 </style>
